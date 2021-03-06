b9e8c6cdecedfc188f0f7cb12abc83238bcf14d6
Ryan Caldwell <ryanjcaldwell4@gmail.com>
2021-03-06 16:39:49 -0500

    Minor UI updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   def render_404
     render file: "#{Rails.root.join}/public/404.html", status: :not_found
   end
+
+  def after_sign_in_path_for(resource)
+    projects_path
+  end
 end
