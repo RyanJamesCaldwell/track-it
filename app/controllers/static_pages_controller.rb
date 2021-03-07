be43928d263a9cddd8900f34008e5c7294c31b1d
Ryan Caldwell <ryanjcaldwell4@gmail.com>
2021-03-07 17:35:26 -0500

    Base 'Get Started' path on home page on if the user is signed in

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,16 @@
 class StaticPagesController < ApplicationController
   def home
     @user = current_user
+    determine_get_started_path
+  end
+
+  private
+
+  def determine_get_started_path
+    @get_started_destination = if user_signed_in?
+                                 projects_path
+                               else
+                                 new_user_registration_path
+                               end
   end
 end
